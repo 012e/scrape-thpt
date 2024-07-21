0bf91adb5f0279edc8b3323ca4a574300fedb0b1
Pham Nhat Huy <huyphmnhat@gmail.com>
2024-07-21 07:15:57 +0700

    scripts/cal_score: use generated columns

---
+++ scripts/cal_score.sql
@@ -0,0 +1,5 @@
+alter table students add column a00 as (toan + ly + hoa);
+alter table students add column a01 as (toan + ly + anh);
+alter table students add column d01 as (toan + van + anh);
+alter table students add column c00 as (van + su + dia);
+alter table students add column b00 as (toan + sinh + hoa);
